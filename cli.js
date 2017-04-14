9fd1e997a1b9425310b10d07b3be0e5c2cc3a11a
John Otander <johnotander@gmail.com>
2017-04-14 09:25:37 -0600

    Implement cli

--- cli.js
+++ cli.js
@@ -1,2 +1,30 @@
+#!/usr/bin/env node
+
+const meow = require('meow')
+const shtml = require('shtml')
+
 const nightShift = require('./')
+
+const cli = meow(shtml`
+  <div>
+    <underline>Usage</underline>
+    $ night-shift<br><br>
+
+    <underline>Options</underline>
+
+    -h, --help - Get help menu
+    -v, --version - Get the version<br><br>
+
+    <underline>Examples</underline>
+
+    $ night-shift -h
+    $ night-shift
+  </div>
+`, {
+  alias: {
+    v: 'version',
+    h: 'help'
+  }
+})
+
 nightShift()
